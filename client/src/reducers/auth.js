b4eacac690130c66c1611aac1a46e47f739025ce
Miguel Gonzalez <[mgonzalezj@bitbucket.org]>
2021-06-18 18:50:10 -0600

    Load user & Auth Token

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -16,6 +18,13 @@ export default function (state = initialState, action) {
     const { type, payload } = action;
     
     switch (type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            };
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);   //Passing obtained token to payload.token
             return {                                        //Returning the new state
@@ -23,16 +32,17 @@ export default function (state = initialState, action) {
                 payload,
                 isAuthenticated: true,
                 loading: false
-            }
+            };
         
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token');
             return {                                        //Returning the new state
                 ...state,
                 token: null,
                 isAuthenticated: false,
                 loading: false
-            }
+            };
         default:
             return state;
     }

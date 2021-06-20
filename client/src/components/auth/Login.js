2fbccb1bed13cf08e728dc3ef14bf4bfd9da2954
Miguel Gonzalez <[mgonzalezj@bitbucket.org]>
2021-06-19 21:15:18 -0600

    Logout & Navbar links

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 //CONNECT TO REDUX
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
 
 const Login = ({ login, isAuthenticated }) => {

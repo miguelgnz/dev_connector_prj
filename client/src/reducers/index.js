c7713cd4eb07f2412c08b89b759e37a77be4aa19
Miguel Gonzalez <[mgonzalezj@bitbucket.org]>
2021-06-23 16:00:52 -0600

    Profile reducer & get current profile

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
 
 
 
 export default combineReducers({
     alert,
-    auth
+    auth,
+    profile
 });

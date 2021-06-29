f53281dcafc7622696047995fbbebef2c3a7deb0
Miguel Gonzalez <[mgonzalezj@bitbucket.org]>
2021-06-28 18:57:45 -0600

    Adding Posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert'
 import {
+    ADD_POST,
     DELETE_POST,
     GET_POSTS,
     POST_ERROR,
@@ -66,7 +67,7 @@ export const removeLike = id => async dispatch => {
 //Delete Post
 export const deletePost = id => async dispatch => {
     try {
-        const res = await axios.delete(`/api/posts//${id}`);
+        const res = await axios.delete(`/api/posts/${id}`);
 
         dispatch({
             type: DELETE_POST,
@@ -74,6 +75,32 @@ export const deletePost = id => async dispatch => {
         });
 
         dispatch(setAlert('Post Removed', 'success'))
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+
+//Add Post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.post('/api/posts/', formData, config);
+
+        dispatch({
+            type: ADD_POST,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Post Created', 'success'))
     } catch (err) {
         dispatch({
             type: POST_ERROR,

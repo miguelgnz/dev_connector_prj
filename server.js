795edce44f667e557af5e2c6ed79626544d4b986
Miguel González <mgx.gon@gmail.com>
2024-10-24 07:35:16 -0600

    Updating dependencies and some imports

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
